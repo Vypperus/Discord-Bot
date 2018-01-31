28e4a414103688cd29fe682bd9519067e2e38d8c
Vypp3rus <xyzetmusic@gmail.com>
2018-01-31 21:08:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -107,3 +107,7 @@ let serverembed = new Discord.RichEmbed()
 return message.channel.send(serverembed); 
 } 
 });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
