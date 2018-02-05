b139a78d0fc6936ec595a84eb47137f418636ae3
Vypp3rus <xyzetmusic@gmail.com>
2018-02-05 14:26:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //import { Message } from 'discord.js'; 
 
-var port = process.env.PORT || 8000
+//var port = process.env.PORT || 8000
 
 const Discord = require('discord.js'); 
 const robot = new Discord.Client({ 
